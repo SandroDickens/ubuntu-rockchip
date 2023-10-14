84a305ecdac26174199e62b2e6bac4ad52fd1e14
Dickens <sandrodickens@gmail.com>
2023-10-14 10:49:33 +0800

    Update build-kernel.sh

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -20,7 +20,7 @@ if [[ "${MAINLINE}" != "Y" ]]; then
 
     rm -f ../*.buildinfo ../*.changes
 else
-    test -d linux ||  git clone --single-branch --progress -b v6.5-rk3588 https://github.com/SandroDickens/linux-Joshua-Riek.git --depth=1 linux
+    test -d linux ||  git clone --single-branch --progress -b v6.5-rk3588 https://github.com/SandroDickens/linux-rockchip-v6.git --depth=1 linux
     cd linux
 
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rockchip_linux_defconfig
