b2605ac2a1836b43a586031e2c7bbc1f58a75ce7
Dickens <sandrodickens@gmail.com>
2023-10-15 18:40:05 +0800

    Use rockchip kernel

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -12,11 +12,13 @@ cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
 mkdir -p build && cd build
 
 if [[ "${MAINLINE}" != "Y" ]]; then
-    test -d linux-rockchip || git clone --single-branch --progress -b rk-5.10-rkr6 https://github.com/SandroDickens/linux-rockchip-armbian.git --depth=1 linux-rockchip
+    # test -d linux-rockchip || git clone --single-branch --progress -b rk-5.10-rkr6 https://github.com/SandroDickens/linux-rockchip-armbian.git --depth=1 linux-rockchip
+    test -d linux-rockchip || git clone --single-branch --progress -b nanopi5-v5.10.y_opt https://github.com/friendlyarm/kernel-rockchip --depth=1 linux-rockchip
     cd linux-rockchip
 
     # Compile kernel into a deb package
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rockchip_linux_defconfig
+    # make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rockchip_linux_defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- nanopi6_linux_defconfig
     make KBUILD_IMAGE="arch/arm64/boot/Image" CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 -j "$(nproc)" bindeb-pkg
 
     rm -f ../linux-image-*dbg*.deb ../linux-libc-dev_*.deb ../*.buildinfo ../*.changes ../*.dsc ../*.tar.gz
