fec0f7729fec9632772d392c308e2f8b977d8ffe
Dickens <sandrodickens@gmail.com>
2023-10-22 00:02:11 +0800

    Update build-kernel.sh

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -16,7 +16,7 @@ if [[ "${MAINLINE}" != "Y" ]]; then
     cd linux-rockchip
 
     # Compile kernel into a deb package
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rockchip_linux_defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- nanopi6_linux_defconfig
     make KBUILD_IMAGE="arch/arm64/boot/Image" CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 -j "$(nproc)" bindeb-pkg
 
     rm -f ../linux-image-*dbg*.deb ../linux-libc-dev_*.deb ../*.buildinfo ../*.changes ../*.dsc ../*.tar.gz
